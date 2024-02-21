bf451c8afd0687305c4c14e28613a1fb62b1c9f4
YONGHWA\ekore <yonghwa95@naver.com>
2024-02-21 18:21:45 +0900

    복습완료

---
+++ src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,28 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+        assertThat(conversionService.convert("1000", Integer.class)).isEqualTo(1000);
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1000");
+        assertThat(conversionService.convert("127.0.0.1:8080", IpPort.class)).isEqualTo(new IpPort("127.0.0.1", 8080));
+        assertThat(conversionService.convert(new IpPort("127.0.0.1", 8080), String.class)).isEqualTo("127.0.0.1:8080");
+
+    }
+}
